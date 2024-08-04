2ae6fa2aab353da371d6ab51d1c0bfb57b48a519
rishadpt <rishadpt16@gmail.com>
2024-08-04 18:07:03 +0530

    url changed

--- src/app/axiosInstance.ts
+++ src/app/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5200/v2/',
+  baseURL: 'https://api.apescommunity.com/v2/',
   // baseURL: 'https://api.splicex.ae/api/v1/',
   headers: {
     'Content-Type': 'application/json',
